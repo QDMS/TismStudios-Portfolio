c03c4f938ccb61a5b2e42ba78f4873542436b7b5
Qujuan Miller <qujuan.miller@snhu.edu>
2023-04-16 10:23:37 -0400

    Finish Adding Music Player

--- src/sections/musicplayer/MusicPlayer.jsx
+++ src/sections/musicplayer/MusicPlayer.jsx
@@ -7,20 +7,35 @@ import Player from "./Player";
 const MusicPlayer = () => {
   const [songs, setSongs] = useState(audios);
   const [currentIndex, setCurrentIndex] = useState(null);
-  const [currentSong, setCurrentSong] = useState({});
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   const getSongData = (song, index) => {
-    setCurrentIndex(index)
+    setCurrentIndex(index);
     setCurrentSong(song);
+  };
+  const nextSong = () => {
+    setCurrentIndex(currentIndex + 1);
+    setCurrentSong(audios[currentIndex + 1]);
+  };
+  const prevSong = () => {
+    setCurrentIndex(currentIndex - 1);
+    setCurrentSong(audios[currentIndex - 1]);
   };
   return (
     <section id="musicPlayer">
       <>
         <div className="player-main">
-          <Player currentSong={currentSong} currentIndex={currentIndex}/>
+          <Player
+            currentSong={currentSong}
+            currentIndex={currentIndex}
+            nextSong={nextSong}
+            prevSong={prevSong}
+          />
         </div>
         <div className="app-main" data-aos="fade-up">
           {songs.map((song, index) => {
-            return <AudioFiles song={song} getSongData={getSongData} index={index}/>;
+            return (
+              <AudioFiles song={song} getSongData={getSongData} index={index} />
+            );
           })}
         </div>
       </>
--- src/sections/musicplayer/Player.js
+++ src/sections/musicplayer/Player.js
@@ -5,47 +5,46 @@ import {
   BsFillSkipBackwardCircleFill,
   BsFillSkipForwardCircleFill,
 } from "react-icons/bs";
+import Speaker from "../../assets/speaker.gif";
 
-export default function Player({ currentSong, currentIndex }) {
+export default function Player({
+  currentSong,
+  currentIndex,
+  nextSong,
+  prevSong,
+}) {
   const [isPlaying, setIsplaying] = useState(false);
   const audioRef = useRef(null);
   const togglePlay = () => {
     setIsplaying(!isPlaying);
   };
   useEffect(() => {
-  if (isPlaying) {
-    audioRef.current.play();
-  }
-  else {
-    audioRef.current.pause();
-  }
-  }, [isPlaying, currentIndex])
-  
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying, currentIndex]);
+
   return (
     <div>
-      <audio ref={audioRef}
-      src={currentSong.music}
-      >
-
-      </audio>
+      <audio ref={audioRef} src={currentSong.music}></audio>
       <h1 style={{ textAlign: "center", marginBottom: 10 }}>
         TismStudio Music Player
       </h1>
       <div className="player-card">
-        {currentSong ? (
-          <div>
-            <h3 className="activeSong-name">{currentSong.name}</h3>
-            <h5 className="activeArtist-name">{currentSong.creator}</h5>
-          </div>
-        ) : (
-          ""
-        )}
+        <div className="image-container">
+        <img className="speaker-image" src={Speaker} alt='Speaker'/>
+        </div>
+        <h2 className="activeSong-name">{currentSong.name}</h2>
+        <h4 className="activeArtist-name">{currentSong.creator}</h4>
 
         <div className="control-icons">
           <BsFillSkipBackwardCircleFill
             color="white"
             className="icons"
             size={50}
+            onClick={prevSong}
           />
 
           {isPlaying ? (
@@ -68,6 +67,7 @@ export default function Player({ currentSong, currentIndex }) {
             color="white"
             className="icons"
             size={50}
+            onClick={nextSong}
           />
         </div>
       </div>
